2f9a4eb7444bb4e1e7197adf99ce6df2edb6a64b
Chuda4ok <98989928+Chuda4ok@users.noreply.github.com>
2022-02-04 11:08:43 +0200

    Update values.js

--- access/values.js
+++ access/values.js
@@ -1,17 +1,17 @@
 // Активировать документы (true - включены, false - отключены)
-var isRightsEnabled = true; // Права
+var isRightsEnabled = false; // Права
 var isCovidCertificateEnabled = true; // COVID-сертификат
 
 // Общее
-var birthdate = "01.01.2001"; // Дата рождения
-var fullname = "Фамилия Имя Отчество" // ФИО
-var name = "Имя" // Имя
+var birthdate = "18.03.2003"; // Дата рождения
+var fullname = "Уманець Олександр Олександрович" // ФИО
+var name = "Олександр" // Имя
 
 // Паспорт
-var passport_id = "123456789" // Номер паспорта
+var passport_id = "769873542" // Номер паспорта
 
 // КПП
-var kpp_id = "0123456789" // ИНН
+var kpp_id = "5387457647" // ИНН
 
 // Права
 var rights_valid_until = "01.01.2022" // Права "Дійсні до"
@@ -20,5 +20,5 @@ var rights_tsc = "ТСЦ 8631" // Права "Видав
 var rights_id = "KBE968639" // Права "Серія та номер""
 
 // COVID-сертификат
-var covid_valid_until = "01.01.2022" // Сертификат "Дійсний до"
+var covid_valid_until = "11.04.2022" // Сертификат "Дійсний до"
 var covid_certificate_id = "URN:UVCI:01:UA:0<br>E55669376876888<br>B9E2520C4F88930" // Номер сертификата (<br> - перенос строки)
